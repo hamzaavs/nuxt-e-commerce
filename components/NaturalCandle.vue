733798cf11158a7f0013943f975af9f3a16bcfb7
hamza avsar <avshamza1@gmail.com>
2023-11-24 19:24:56 +0300

    Add Product Card and Naturel Candle section

--- components/NaturalCandle.vue
+++ components/NaturalCandle.vue
@@ -1,13 +1,13 @@
 <template>
   <div
-    class="md:bg-[url('/bg-image.svg')] bg-[url('/natural-candle-mobile.svg')] w-full h-[50rem] md:h-[45rem] bg-fixed bg-cover flex items-center justify-center mb-10">
+    class="md:bg-[url('/bg-image.svg')] bg-[url('/natural-candle-mobile.svg')] h-[50rem] md:h-[45rem] bg-fixed bg-cover flex items-center justify-center mb-10">
     <div class="bg-white w-80 h-96 md:w-[45rem] md:h-80 rounded-sm flex flex-col justify-center items-center ">
       <h2 class="text-5xl">🌱</h2>
       <h2 class="text-black text-center px-4 text-5xl"> The nature candle</h2>
       <p class="text-black text-center px-5 py-5 md:px-28">All handmade with natural soy wax, Candleaf is a companion
         for all your pleasure
         moments </p>
-      <UButton color="green" label="Discovery our collection"/>
+      <UButton color="green" label="Discovery our collection" class="px-16 py-2 text-white"/>
     </div>
   </div>
 </template>
