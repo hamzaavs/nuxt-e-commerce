447c2659c89093fa43d4d9fb91aea688e62b3402
hamza avsar <avshamza1@gmail.com>
2024-01-03 00:43:15 +0300

    Add props

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,73 +1,52 @@
 <script setup lang="ts">
 import MazBtn from "maz-ui/components/MazBtn"
-const productCardValue = [
-  {
-    color: "1",
-    name: "Spiced Mint",
-    price: "9,99$",
-    comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus harum hic iure neque nobis perferendis reprehenderit? Eos, facere, odio?"
+
+const props = defineProps({
+  button_label: {
+    type: String,
+    required: true
   },
-  {
-    color: "2",
-    name: "Sweet Strawberry",
-    price: "9,99$",
-    comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus harum hic iure neque nobis perferendis reprehenderit? Eos, facere, odio?"
+  required: {
+    type: Boolean,
+    default: false
   },
-  {
-    color: "3",
-    name: "Cool Blueberries",
-    price: "9,99$",
-    comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus harum hic iure neque nobis perferendis reprehenderit? Eos, facere, odio?"
+  color: {
+    type: String,
+    default: "1",
+    required: true
   },
-  {
-    color: "4",
-    name: "Juicy Lemon",
-    price: "9,99$",
-    comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus harum hic iure neque nobis perferendis reprehenderit? Eos, facere, odio?"
+  comment: {
+    type: String,
+    default: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus harum hic iure neque nobis perferendis reprehenderit? Eos, facere, odio?",
+    required: true
   },
-  {
-    color: "5",
-    name: "Product Name",
-    price: "9,99$",
-    comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus harum hic iure neque nobis perferendis reprehenderit? Eos, facere, odio?"
+  name: {
+    type: String,
+    default: "Product Name",
+    required: true
   },
-  {
-    color: "6",
-    name: "Fragrant Cinnamon",
-    price: "9,99$",
-    comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus harum hic iure neque nobis perferendis reprehenderit? Eos, facere, odio?"
-  },
-  {
-    color: "7",
-    name: "Summer Cherries",
-    price: "9,99$",
-    comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus harum hic iure neque nobis perferendis reprehenderit? Eos, facere, odio?"
-  },
-  {
-    color: "8",
-    name: "Clean Lavander",
-    price: "9,99$",
-    comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus harum hic iure neque nobis perferendis reprehenderit? Eos, facere, odio?"
+  price: {
+    type: String,
+    default: "9,99$",
+    required: true
   }
-];
+})
 </script>
 
-
 <template>
-
-  <div class="w-11/12 grid grid-cols-1 2xl:grid-cols-4 md:grid-cols-2 gap-4 p-4 m-auto">
-    <div v-for="item in productCardValue" class="w-3/4 p-4 m-auto shadow-lg rounded-2xl">
+  <div>
+    <div class="w-3/4 p-4 m-auto shadow-lg rounded-2xl">
       <div class="bg-gray-100 w-full top-0 p-0 max-w-screen m-auto">
-        <NuxtImg :src="'/product-' + item.color + '.svg'" height="300"/>
+        <NuxtImg :src="'/product-' + props.color + '.svg'" height="300"/>
       </div>
       <div>
-        <h2 class="text-black font-bold pb-3">{{ item.name }}</h2>
-        <p class="text-gray-700">{{ item.comment }}</p>
-        <p class="text-primary text-right mb-2">{{ item.price }}</p>
+        <h2 class="text-black font-bold pb-3">{{ props.name }}</h2>
+        <p class="text-gray-700">{{ props.comment }}</p>
+        <p class="text-primary text-right mb-2">{{ props.price }}</p>
       </div>
     </div>
   </div>
   <div class="w-1/4 visible m-auto md:invisible">
-    <MazBtn color="success">See More</MazBtn>
+    <MazBtn color="success" v-if="props.required">{{props.button_label}}</MazBtn>
   </div>
 </template>
--- components/ProductInfo.vue
+++ components/ProductInfo.vue
@@ -1,10 +1,70 @@
 <script setup lang="ts">
 import ProductCard from "/components/ProductCard.vue";
 import ContainerLayout from "~/layout/ContainerLayout.vue";
+const productCardValue = [
+  {
+    color: "1",
+    name: "Spiced Mint",
+    price: "9,99$",
+    comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus harum hic iure neque nobis perferendis reprehenderit? Eos, facere, odio?"
+  },
+  {
+    color: "2",
+    name: "Sweet Strawberry",
+    price: "9,99$",
+    comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus harum hic iure neque nobis perferendis reprehenderit? Eos, facere, odio?"
+  },
+  {
+    color: "3",
+    name: "Cool Blueberries",
+    price: "9,99$",
+    comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus harum hic iure neque nobis perferendis reprehenderit? Eos, facere, odio?"
+  },
+  {
+    color: "4",
+    name: "Juicy Lemon",
+    price: "9,99$",
+    comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus harum hic iure neque nobis perferendis reprehenderit? Eos, facere, odio?"
+  },
+  {
+    color: "5",
+    name: "Product Name",
+    price: "9,99$",
+    comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus harum hic iure neque nobis perferendis reprehenderit? Eos, facere, odio?"
+  },
+  {
+    color: "6",
+    name: "Fragrant Cinnamon",
+    price: "9,99$",
+    comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus harum hic iure neque nobis perferendis reprehenderit? Eos, facere, odio?"
+  },
+  {
+    color: "7",
+    name: "Summer Cherries",
+    price: "9,99$",
+    comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus harum hic iure neque nobis perferendis reprehenderit? Eos, facere, odio?"
+  },
+  {
+    color: "8",
+    name: "Clean Lavander",
+    price: "9,99$",
+    comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus harum hic iure neque nobis perferendis reprehenderit? Eos, facere, odio?"
+  }
+];
+
 </script>
 
 <template>
-  <ContainerLayout title="Products" description="Order it for you or for your beloved ones">
-    <ProductCard />
+  <ContainerLayout title="Products" description="Order it for you or for your beloved ones" >
+    <div class="w-11/12 grid grid-cols-1 2xl:grid-cols-4 md:grid-cols-2 gap-4 p-4 m-auto">
+      <div v-for="item in productCardValue">
+        <ProductCard
+          button_label="See More"
+          :color="item.color" :comment="item.comment" :name="item.name"
+          :price="item.price"
+          required
+        />
+      </div>
+    </div>
   </ContainerLayout>
 </template>
